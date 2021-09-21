a0c165e2a7257b5aa4a3ccfbea35516715434bf8
Aditya Shah <aditya7395@gmail.com>
2021-09-21 10:48:56 -0700

    Added q3 with proper styling

--- js/script.js
+++ js/script.js
@@ -28,3 +28,24 @@ function resetQuestion2(){
 	document.getElementById('q2option3').checked=false;
 	document.getElementById('q2option4').checked=false;
 }
+
+function submitQuestion3(){
+	window.location.href = "question4.html";	  	
+}
+
+function resetQuestion3(){
+	document.getElementById('q3option1m1').checked=false;
+	document.getElementById('q3option1m2').checked=false;
+	document.getElementById('q3option2m1').checked=false;
+	document.getElementById('q3option2m2').checked=false;
+	document.getElementById('q3option3m1').checked=false;
+	document.getElementById('q3option3m2').checked=false;
+}
+
+function setMark1Value(id){
+	document.getElementById('price').innerHTML='$'+document.getElementById(id).value;
+}
+
+function setMark2Value(id){
+	document.getElementById('company-name').innerHTML=document.getElementById(id).value;
+}
