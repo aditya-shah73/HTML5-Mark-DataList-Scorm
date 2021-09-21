e48cc5b539bf35fda43767543c2560d837920474
Aditya Shah <aditya7395@gmail.com>
2021-09-20 17:13:27 -0700

    Added q1 and q2 with proper styling

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ function userLogin(){
 		window.location.href = "question1.html";
 	}
 }
+
+function submitQuestion1(){
+	window.location.href = "question2.html";	  	
+}
+
+function resetQuestion1(){
+	document.getElementById('q1option1').checked=false;
+	document.getElementById('q1option2').checked=false;
+	document.getElementById('q1option3').checked=false;
+	document.getElementById('q1option4').checked=false;
+}
+
+function submitQuestion2(){
+	window.location.href = "question3.html";	  	
+}
+
+function resetQuestion2(){
+	document.getElementById('q2option1').checked=false;
+	document.getElementById('q2option2').checked=false;
+	document.getElementById('q2option3').checked=false;
+	document.getElementById('q2option4').checked=false;
+}
